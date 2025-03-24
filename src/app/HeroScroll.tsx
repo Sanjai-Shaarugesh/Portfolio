d8914718cccf772c3cb8419035c4675f72ab50f6
Sanjai-Shaarugesh <shaarugesh5@outlook.com>
2025-03-24 17:49:12 +0530

    first commit 🛸

---
+++ src/app/HeroScroll.tsx
@@ -0,0 +1,37 @@
+"use client";
+import React from "react";
+import { ContainerScroll } from "../components/ui/container-scroll-animation";
+import Image from "next/image";
+
+type HeroScrollProps = {
+  title: string;
+  description: string;
+};
+
+export function HeroScroll({title, description}: HeroScrollProps) {
+  return (
+    <div className="flex flex-col overflow-hidden">
+      <ContainerScroll
+        titleComponent={
+          <>
+            <h1 className="text-4xl font-semibold text-black dark:text-white">
+              {title} <br />
+              <span className="text-4xl md:text-[6rem] font-bold mt-1 leading-none">
+               {description}
+              </span>
+            </h1>
+          </>
+        }
+      >
+        <Image
+          src={`/github.png`}
+          alt="hero"
+          height={720}
+          width={1400}
+          className="mx-auto rounded-2xl object-cover h-full object-left-top"
+          draggable={false}
+        />
+      </ContainerScroll>
+    </div>
+  );
+}
